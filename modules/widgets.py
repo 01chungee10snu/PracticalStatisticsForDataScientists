5c2b2275923190df038ba6711101cf16a678d4c0
01chungee10snu <brienz311@snu.ac.kr>
2025-06-26 21:58:21 +0900

    Remove example images and use iris dataset

--- modules/widgets.py
+++ modules/widgets.py
@@ -7,9 +7,9 @@ def create_interactive_demo():
     """사용자가 입력한 데이터 크기에 따라 그래프를 업데이트"""
 
     def update(size):
-        data = data_processing.generate_statistics_data(size)
-        img = visualization.plot_statistics(data)
+        data = data_processing.sample_public_dataset(size)
+        img = visualization.plot_iris_example(data)
         display(HTML(f'<img src="data:image/png;base64,{img}"/>'))
 
-    interact(update, size=IntSlider(min=10, max=100, step=10, value=50))
+    interact(update, size=IntSlider(min=10, max=150, step=10, value=50))
 
